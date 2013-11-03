ee4bebb0830dda80a90463f42893295f0478de6a
Naoto Yoshioka <yos@pb3.so-net.ne.jp>
2013-11-03 13:59:39 +0900

    -n Initial Commit

---
+++ Foldable/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Foldable
+//
+//  Created by Naoto Yoshioka on 2013/11/03.
+//  Copyright (c) 2013年 Naoto Yoshioka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
